85aada2a19caa7a5d2c8c9b214a5b3b2ff6f798c
hsy <reservedcalender@gmail.com>
2020-11-02 00:13:25 +0900

    실험가능 but 미완성 수정/보완 필요

---
+++ PersonalInformationDetector/src/main/java/Identifier.java
@@ -0,0 +1,20 @@
+public class Identifier {
+    String name;
+    String pttn;
+    Validator valid;
+
+    public Identifier(String name, String pttn, Validator valid) {
+        this.name = name;
+        this.pttn = pttn;
+        this.valid = valid;
+    }
+
+    @Override
+    public String toString() {
+        return "Idf(" + name + ")";
+    }
+
+    interface Validator {
+        boolean check(String... groups);
+    }
+}
---
+++ PersonalInformationDetector/src/main/java/Main.java
@@ -0,0 +1,19 @@
+//import org.apache.tika.exception.TikaException;
+//import org.xml.sax.SAXException;
+//
+//import java.io.IOException;
+//
+//public class Main {
+//    public static void main(String[] args) throws IOException, SAXException, TikaException {
+//        String fileUrl = "/Users/KYUMIN/pjt/test3.hwp";
+//        String parseString = new Extractor().extract(fileUrl);
+//
+//        new Validation().validate(parseString, "UTF-8");
+//    }
+//}
+
+
+///////////////////////
+// Validation 아래 main으로 일단 실행
+// Tika는 전체적인 실행된 이후에 생각
+///////////////////////
