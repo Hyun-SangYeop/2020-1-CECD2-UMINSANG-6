b1111e5fc55ab137a5120440da354a59da373449
hsy <reservedcalender@gmail.com>
2020-11-19 14:21:29 +0900

    리눅스 서버 시도

--- app-server/src/main/java/me/zlarbals/appserver/firebaseController.java
+++ app-server/src/main/java/me/zlarbals/appserver/firebaseController.java
@@ -10,14 +10,15 @@ public class firebaseController {
     //public String fcmtest(HttpServletRequest request, HttpServletResponse response, Model model){
 
     @GetMapping("/test")
-    public String fcmtest(@RequestParam String filepath,@RequestParam String ssn,@RequestParam String mph,@RequestParam String phn,@RequestParam String hin){
+    public String fcmtest(@RequestParam String filepath){
 
         FcmUtil fcmUtil=new FcmUtil();
 
 
         //String tokenId="cEnZCfB2vEQ:APA91bGroHMtH3oWEZKuZftjanwmZ5RR_TU4dOpLADLv47dAr5AI262QUAhw45vPjq6sak4t1v0AulIxLzltEX6pxJxYFzbyB929Cvu6MD7tPYeG7ScStMxdubo_OAKpFosskVocBmA1";
         String title="DLP";
-        String content=filepath+"\n 복사/이동이 감지되었습니다. \n ssn="+ssn+" mph="+mph+" phn="+phn+" hin="+hin;
+        String content=filepath+"\n 복사/이동이 감지되었습니다.";
+        // \n ssn="+ssn;" mph="+mph+" phn="+phn+" hin="+hin;
 
         fcmUtil.send_FCM(title,content);
         System.out.println(filepath);
